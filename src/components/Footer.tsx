024089191eeb3575143628c36090ec6f2188b9a8
DiedeKlink <diede@protonmail.com>
2024-09-10 21:38:48 +0200

    Complete Setup (Components, Radix UI Icons)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://bytegrad.com">
+            ByteGrad.com
+          </a>
+          . Intended for learning or your portfolio
+        </p>
+        <p>
+          <small className="u-bold u-italic">Not allowed</small> to use as your
+          own teaching material
+        </p>
+      </small>
+    </footer>
+  );
+}
